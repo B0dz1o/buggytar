8323afc12787c708d98924ec6fd96b9cbfde4678
Piotr Bogdan <bogdzio@o2.pl>
2016-09-12 23:33:46 +0200

    Two songs added

--- js/main.js
+++ js/main.js
@@ -1,66 +1,33 @@
-var playNotes = function(strums) {
-  var ac = new AudioContext();
-  var tempo = 120;
+ac = new AudioContext();
+
+var playNotes = function(strums, pTempo) {
+  var tempo = pTempo || 120;
   var when = ac.currentTime + 0.5;
-
-  for (i = 0 ; i < strums.length ; ++i) {
-    var sequence = new TinyMusic.Sequence( ac, tempo, strums[i] );
-    sequence.loop = false;
-    sequence.staccato = 0.5;
-    sequence.waveType = 'sine';
-    sequence.play(when + i / 20);
-  }
-}
-
-var addHarmonics = function(tabs) {
-  var strums = [];
-  for (i = 0; i < tabs.length; ++i) {
-    strums.push([tabs[i]]);
-    oct = parseInt(tabs[i][1]);
-    // strums.push([tabs[i][0] + (oct+1) + tabs[i].substr(2,2)]);
-  }
-  playNotes(strums);
+  var sequence = new TinyMusic.Sequence( ac, tempo, strums );
+  sequence.loop = false;
+  sequence.staccato = 0.2;
+  sequence.waveType = 'sawtooth';
+  sequence.play(when);
 }
 
 openGuitar = [
-  'E3 q',
-  'A3 q',
-  'D4 q',
-  'G4 q',
-  'B4 q',
-  'E5 q'
+  'E2 q',
+  'A2 q',
+  'D3 q',
+  'G3 q',
+  'B3 q',
+  'E4 q'
 ];
 
-guitar = [
-  'E3 q',
-  'A#3 q',
-  'E4 q',
-  'A4 q',
-  'B4 q',
-  'E5 q'
-];
 
-guitar2 = [
-  'E4 q',
-  'A4 q',
-  'D5 q',
-  'G5 q',
-  'B5 q',
-  'E6 q'
-];
 // 'B#-C|C#-Db|D|D#-Eb|E-Fb|E#-F|F#-Gb|G|G#-Ab|A|A#-Bb|B-Cb',
-gfather = ['B3 q', 'E4 q', 'G4 q', 'F# q', 'E4 q', 'G4 q', 'E4 q', 'F#4 q'];
-
-addHarmonics(guitar);
-
-var ac = new AudioContext();
-var tempo = 120;
-var when = ac.currentTime + 0.5;
-
-// var sequence = new TinyMusic.Sequence( ac, tempo, guitar );
-sequence.loop = false;
-sequence.waveType = 'sine';
-sequence.play(when);
-
-// addHarmonics(guitar2);
-// playchord(guitar2);
+gfather1 = ['B3 q', 'E4 q', 'G4 q', 'F#4 q', 'E4 q', 'G4 q', 'E4 q', 'F#4 q', 'E4 q',
+  'C4 q', 'D4 q', 'B3 q', '- h'];
+gfather2 = gfather1.slice(0,-4).concat(['B3 q', 'A#3 q', 'A3 q']);
+house_rising1 = aMin.slice(1,4).concat(aMin.slice(0).reverse().slice(0,3));
+house_rising2 = cMaj.slice(1,4).concat(aMin.slice(0).reverse().slice(0,3));
+house_rising3 = dMaj.slice(2,5).concat(dMaj.slice(0).reverse().slice(0,3));
+house_rising4 = fMaj.slice(0,1).concat(fMaj.slice(2,4),
+  fMaj.slice(0).reverse().slice(0,3));
+playNotes(gfather1.concat(gfather2));
+playNotes(house_rising1.concat(house_rising2, house_rising3, house_rising4), 180);
