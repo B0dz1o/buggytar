a4ffbe8260f6a137b17e578b7fa383b8e15c55cc
Piotr Bogdan <bogdzio@o2.pl>
2016-09-13 02:09:24 +0200

    Main screen buttons

--- js/guitar.js
+++ js/guitar.js
@@ -1,3 +1,4 @@
+///main screen drawn in canvas
 (function() {
   var canvas = document.getElementById('mainCanvas');
   var context = canvas.getContext('2d');
@@ -5,13 +6,13 @@
   var centerY = canvas.height / 2;
   var radius = 70;
 
-
+  ///background
   context.beginPath();
   context.rect(0, 0, 2 * centerX, 2* centerY);
   context.fillStyle = "grey";
   context.fill();
 
-
+  ///guitar body (small)
   context.beginPath();
   context.arc(centerX, centerY, 1.3* radius, 0, 2 * Math.PI, false);
   context.fillStyle = 'saddlebrown';
@@ -20,7 +21,7 @@
   context.strokeStyle = '#003300';
   context.stroke();
 
-
+  ///guitar body (big)
   context.beginPath();
   context.arc( 0.6 * centerX, 1.4 * centerY, 2*radius, 0, 2 * Math.PI, false);
   context.fillStyle = 'saddlebrown';
@@ -29,6 +30,7 @@
   context.strokeStyle = '#003300';
   context.stroke();
 
+  ///guitar body (hollow)
   context.beginPath();
   context.arc( 0.6 * centerX, 1.4 * centerY, 0.6*radius, 0, 2 * Math.PI, false);
   context.fillStyle = 'black';
@@ -37,10 +39,37 @@
   context.strokeStyle = 'brown';
   context.stroke();
 
+  ///guitar neck
   context.save();
   context.translate(canvas.width / 2, canvas.height / 2);
   context.rotate(Math.PI / 4);
   context.fillStyle = 'black';
   context.fillRect(-20, -300, 40, 500);
+  for (i = 0 ; i < 35 ; ++i ) {
+    context.fillStyle = i%3? 'white' : 'navy';
+    context.fillRect(-18 , -300 + 10 * i, 35, 8);
+  }
   context.restore();
+
+  /// answers
+  var button = function(num, color) {
+    i = num;
+    context.beginPath()
+    context.rect(0.4* i * centerX, 1.9 * centerY,0.4 * centerX, 0.1 * centerY);
+    context.fillStyle = color || 'white';
+    context.strokeStyle = 'grey';
+    context.fill();
+    context.stroke();
+  }
+  for (i = 0 ; i < 5; ++i) {
+    button(i, 'white');
+  }
+
+  canvas.addEventListener('click', function (event) {
+    if (event.pageY > 1.9 * centerY) {
+      var butNum = Math.floor(event.pageX / (0.4 * centerX));
+      button(butNum,'green');
+    }
+  });
+
 })();
